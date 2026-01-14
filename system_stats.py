f550cfb7b943729857546ec3ea8773c45e29d888
techvyana20-oss <techvyana2.0@gmail.com>
2026-01-14 20:43:20 +0530

    Update system_stats.py

--- system_stats.py
+++ system_stats.py
@@ -1,1 +1,10 @@
+# system_stats.py
+import psutil
+
+def get_cpu():
+    return f"CPU: {psutil.cpu_percent()}%"
+
+def get_ram():
+    mem = psutil.virtual_memory()
+    return f"RAM: {mem.percent}%"
 
